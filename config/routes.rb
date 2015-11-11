e76e84a72392273138fd6b87fe18ed63b0d6ee8a
OnlyCZY <853852998@qq.com>
2015-11-11 11:06:38 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  # 参赛评委
   devise_for :judges
+
+  # 比赛说明页面
+  root 'home#index'
+
+  # 参赛老师
+  resources :players
+
+  # 评分
+  resources :grades, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
