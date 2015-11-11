e403d7cd161bc2fc31e369146176fdcbb747df66
zhangliugang <www.zhangyi508@gmail.com>
2015-11-11 11:24:58 +0800

    change grade

--- db/migrate/20151111031937_create_grades.rb
+++ db/migrate/20151111031937_create_grades.rb
@@ -1,10 +1,10 @@
 class CreateGrades < ActiveRecord::Migration
   def change
     create_table :grades do |t|
-      t.int :target
-      t.int :content
-      t.int :plan
-      t.int :express
+      t.integer :target
+      t.integer :content
+      t.integer :plan
+      t.integer :express
       t.references :judge, index: true, foreign_key: true
       t.references :player, index: true, foreign_key: true
 
