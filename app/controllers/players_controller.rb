e25a1454ab27eeb5f3681b02e9b804553ee79030
OnlyCZY <853852998@qq.com>
2015-11-13 08:25:01 +0800

    finish players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,12 @@
 class PlayersController < BaseController
+	respond_to :html, :js
+
 	def index
 		@players = Player.all
+	end
+
+	def show
+		@player = Player.find(params[:id])
+		respond_with @player
 	end
 end
